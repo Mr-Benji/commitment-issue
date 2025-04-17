0595aa9e99844798dc392341cea6f0e14ad4ae22
NTWARI Emery <ntwariemery007@gmail.com>
2025-04-17 14:08:22 +0300

    Task 8 changes

--- commitment-issues/insert_data.sql
+++ commitment-issues/insert_data.sql
@@ -5,20 +5,20 @@ INSERT INTO sales_transactions (transaction_id, product_name, category, branch_n
   (2, 'Ibuprofen 200mg', 'Pain Relief', 'Kigali Branch', 1500, TO_DATE('2025-04-02', 'YYYY-MM-DD'));
 
 INSERT INTO sales_transactions (transaction_id, product_name, category, branch_name, sale_amount, sale_date) VALUES
-(3, 'Amoxicillin 250mg', 'Antibiotic', 'Kigali Branch', 2500, TO_DATE('2025-04-03', 'YYYY-MM-DD'));
+  (3, 'Amoxicillin 250mg', 'Antibiotic', 'Kigali Branch', 2500, TO_DATE('2025-04-03', 'YYYY-MM-DD'));
 
 INSERT INTO sales_transactions (transaction_id, product_name, category, branch_name, sale_amount, sale_date) VALUES
-(4, 'Amoxicillin 250mg', 'Antibiotic', 'Musanze Branch', 2600, TO_DATE('2025-04-03', 'YYYY-MM-DD'));
+  (4, 'Amoxicillin 250mg', 'Antibiotic', 'Musanze Branch', 2600, TO_DATE('2025-04-03', 'YYYY-MM-DD'));
 
 INSERT INTO sales_transactions (transaction_id, product_name, category, branch_name, sale_amount, sale_date) VALUES
-(5, 'Vitamin D 400IU', 'Supplements', 'Kigali Branch', 1700, TO_DATE('2025-04-04', 'YYYY-MM-DD'));
+  (5, 'Vitamin D 400IU', 'Supplements', 'Kigali Branch', 1700, TO_DATE('2025-04-04', 'YYYY-MM-DD'));
 
 INSERT INTO sales_transactions (transaction_id, product_name, category, branch_name, sale_amount, sale_date) VALUES
-(6, 'Cough Syrup', 'Cough & Cold', 'Kigali Branch', 1800, TO_DATE('2025-04-05', 'YYYY-MM-DD'));
+  (6, 'Cough Syrup', 'Cough & Cold', 'Kigali Branch', 1800, TO_DATE('2025-04-05', 'YYYY-MM-DD'));
 
 INSERT INTO sales_transactions (transaction_id, product_name, category, branch_name, sale_amount, sale_date) VALUES
 <<<<<<< HEAD
-(7, 'Paracetamol 500mg', 'Pain Relief', 'Huye Branch', 1259, TO_DATE('2025-04-04', 'YYYY-MM-DD'));
+  (7, 'Paracetamol 500mg', 'Pain Relief', 'Huye Branch', 1259, TO_DATE('2025-04-04', 'YYYY-MM-DD'));
 =======
-(7, 'Paracetamol 500mg', 'Pain Relief', 'Huye Branch', 1255, TO_DATE('2025-04-04', 'YYYY-MM-DD'));
+  (8, 'Paracetamol 500mg', 'Pain Relief',   'Musanze Branch', 1255, TO_DATE('2025-04-04', 'YYYY-MM-DD'));
 >>>>>>> 90f93f26ee59af017372f828489d50eae01bf0f9
