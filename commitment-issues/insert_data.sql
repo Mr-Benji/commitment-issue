e4f0f1294997e0887db16a9488d22ff4731e0aa9
NTWARI Emery <ntwariemery007@gmail.com>
2025-04-17 22:44:59 +0200

    Update ends

--- commitment-issues/insert_data.sql
+++ commitment-issues/insert_data.sql
@@ -17,8 +17,8 @@ INSERT INTO sales_transactions (transaction_id, product_name, category, branch_n
   (6, 'Cough Syrup', 'Cough & Cold', 'Kigali Branch', 1800, TO_DATE('2025-04-05', 'YYYY-MM-DD'));
 
 INSERT INTO sales_transactions (transaction_id, product_name, category, branch_name, sale_amount, sale_date) VALUES
-<<<<<<< HEAD
   (7, 'Paracetamol 500mg', 'Pain Relief', 'Huye Branch', 1259, TO_DATE('2025-04-04', 'YYYY-MM-DD'));
-=======
+
+INSERT INTO sales_transactions (transaction_id, product_name, category, branch_name, sale_amount, sale_date) VALUES
   (8, 'Paracetamol 500mg', 'Pain Relief',   'Musanze Branch', 1255, TO_DATE('2025-04-04', 'YYYY-MM-DD'));
->>>>>>> 90f93f26ee59af017372f828489d50eae01bf0f9
+
